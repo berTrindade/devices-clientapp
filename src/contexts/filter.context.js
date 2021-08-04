eb49b6ef4d364c9e25cd5f4c1ac409e60a40294c
David Moreno <davidmm@everymundo.com>
2021-08-03 22:18:51 -0400

    default sort type

--- src/contexts/filter.context.js
+++ src/contexts/filter.context.js
@@ -5,7 +5,7 @@ const FilterContext = createContext();
 
 export const FilterProvider = ({children}) => {
   const [deviceTypes, setDeviceTypes] = useState([]);
-  const [sortBy, setSortBy] = useState('');
+  const [sortBy, setSortBy] = useState('type');
   const [direction, setDirection] = useState(SORT_DIRECTION_ASC); 
   return (
     <FilterContext.Provider value={{
