8ee4483b5dc0c411e0e0b0a24ee6040bf0c003db
David Moreno <davidmm@everymundo.com>
2021-08-03 22:18:11 -0400

    add topbar

---
+++ src/component/TopBar.js
@@ -0,0 +1,11 @@
+const TopBar = () => {
+  return (
+    <div
+    class="w-full bg-gray-600 h-16 fixed top-0 animated z-40 shadow-lg px-4"
+  >
+    <div className="flex w-full max-w-3xl mx-auto items-center h-16 text-white text-2xl font-bold">Devices ClientApp</div>
+  </div>
+  );
+};
+
+export default TopBar;
