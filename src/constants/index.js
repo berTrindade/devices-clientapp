3b6efd569237e4370fa0562a310fa5151625f90a
David Moreno <davidmm@everymundo.com>
2021-08-06 20:04:37 -0400

    constants refactoring

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,22 @@
-export const WINDOWS_WORKSTATION = 'WINDOWS_WORKSTATION';
-export const WINDOWS_SERVER = 'WINDOWS_SERVER';
-export const MAC = 'MAC';
-export const SORT_DIRECTION_ASC = 'ASC';
-export const SORT_DIRECTION_DESC = 'DESC';
+export const DEVICES_OPTIONS = {
+  WINDOWS_WORKSTATION: 'WINDOWS_WORKSTATION',
+  WINDOWS_SERVER: 'WINDOWS_SERVER',
+  MAC: 'MAC',
+};
+
+export const SORTING_OPTIONS = [
+  {label: 'System Name', value: 'system_name'},
+  {label: 'HDD Capacity', value: 'hdd_capacity'},
+  {label: 'type', value: 'type'},
+];
+export const DIRECTION = {
+  SORT_DIRECTION_ASC: 'ASC',
+  SORT_DIRECTION_DESC: 'DESC',
+};
+
+export const INITIAL_DEVICE_DATA = {
+  id: '',
+  system_name: '',
+  type: '',
+  hdd_capacity: '0'
+}
